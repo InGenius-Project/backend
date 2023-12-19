deb51af3560d6ce97dc00fc14463ca79dab73ae2
DACHXY <Danny10132024@gmail.com>
2023-12-19 19:39:37 +0800

    Add CheckAndGetUserAsync function in UserService

--- Services/UserService/UserService.cs
+++ Services/UserService/UserService.cs
@@ -1,8 +1,10 @@
-﻿using IngBackend.Interfaces.Repository;
+﻿using IngBackend.Exceptions;
+using IngBackend.Interfaces.Repository;
 using IngBackend.Interfaces.UnitOfWork;
 using IngBackend.Models.DBEntity;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
+
 
 namespace IngBackend.Services.UserService;
 
@@ -28,5 +30,10 @@ public class UserService : Service<User, Guid>
         return await query.FirstOrDefaultAsync(e => e.Email == email);
     }
 
+    public async Task<User>? CheckAndGetUserAsync(Guid userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+        return user ?? throw new UserNotFoundException();
+    }
 
 }
