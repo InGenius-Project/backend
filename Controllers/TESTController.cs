05b7082a8a8ef1439fa1b2db03a9eb94e929b114
DACHXY <Danny10132024@gmail.com>
2023-12-21 17:29:07 +0800

    Add Test Controller

---
+++ Controllers/TESTController.cs
@@ -0,0 +1,19 @@
+﻿using IngBackend.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IngBackend.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class TESTController: BaseController
+{
+    public TESTController() { }
+
+    [HttpGet]
+    public string GetGAPassword()
+    {
+        return Helper.GetSAPassword();
+    }
+}
