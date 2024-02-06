f7eedbf617617f18516e20690c5b998d824b37d1
DACHXY <Danny10132024@gmail.com>
2024-02-06 13:39:37 +0800

    fix: remove secret information in appsettings.json

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,5 +1,5 @@
-using MailKit.Security;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 
 namespace IngBackend.Services;
@@ -7,6 +7,7 @@ namespace IngBackend.Services;
 public class EmailService
 {
     private readonly IConfiguration _configuration;
+
     public EmailService(IConfiguration configuration)
     {
         _configuration = configuration;
@@ -15,14 +16,26 @@ public class EmailService
     public async Task SendEmailAsync(string email, string subject, string message)
     {
         var emailMessage = new MimeMessage();
-        emailMessage.From.Add(new MailboxAddress(_configuration["Email:SenderName"], _configuration["Email:SenderAddress"]));
+        emailMessage.From.Add(
+            new MailboxAddress(
+                _configuration["Email:SenderName"],
+                _configuration["Email:SenderAddress"]
+            )
+        );
         emailMessage.To.Add(new MailboxAddress("", email));
         emailMessage.Subject = subject;
         emailMessage.Body = new TextPart("html") { Text = message };
 
         using var client = new SmtpClient();
-        await client.ConnectAsync(_configuration["Email:SmtpServer"], int.Parse(_configuration["Email:Port"]), SecureSocketOptions.StartTls);
-        await client.AuthenticateAsync(_configuration["Email:Username"], _configuration["Email:Password"]);
+        await client.ConnectAsync(
+            _configuration["Email:SmtpServer"],
+            int.Parse(_configuration["Email:Port"]),
+            SecureSocketOptions.StartTls
+        );
+        await client.AuthenticateAsync(
+            _configuration["Secrets:EmailUsername"],
+            _configuration["Secrets:EmailPassword"]
+        );
         var result = await client.SendAsync(emailMessage);
         await client.DisconnectAsync(true);
     }
