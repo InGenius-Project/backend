b7fff5591257df3c339959bbd39f99f053474207
DACHXY <Danny10132024@gmail.com>
2024-04-21 16:42:37 +0800

    refactor: 🎨 refact ChatHub

--- IngBackendApi.Application/Hubs/ChatHub.cs
+++ IngBackendApi.Application/Hubs/ChatHub.cs
@@ -3,58 +3,72 @@
 using System.Security.Claims;
 using IngBackendApi.Application.Interfaces;
 using IngBackendApi.Context;
+using IngBackendApi.Exceptions;
+using IngBackendApi.Interfaces.Repository;
+using IngBackendApi.Interfaces.UnitOfWork;
 using IngBackendApi.Models.DBEntity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 [Authorize]
-public class ChatHub(IngDbContext context, IHttpContextAccessor httpContextAccessor) : Hub<IChatHub>
+public class ChatHub(IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+    : Hub<IChatHub>
 {
-    private readonly IngDbContext _context = context;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IRepository<User, Guid> _userRepository = unitOfWork.Repository<User, Guid>();
     private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
 
-    // public async Task ReceiveMessage(string message)
-    //     => await Clients.All.
+    public async Task SendMessage(string message) => await Clients.All.ReceiveMessage(message);
 
-    public async Task SendMessage(string message)
-        => await Clients.All.ReceiveMessage(message);
+    public async Task SendMessageToCaller(string message) =>
+        await Clients.Caller.ReceiveMessage(message);
 
-    public async Task SendMessageToCaller(string message)
-        => await Clients.Caller.ReceiveMessage(message);
-
-    public async Task SendMessageToGroup(string groupName, string message)
-        => await Clients.Group(groupName).ReceiveMessage(message);
+    public async Task SendMessageToGroup(string groupName, string message) =>
+        await Clients.Group(groupName).ReceiveMessage(message);
 
     public async Task AddGroup(string message, string groupName)
     {
-        var userIdClaim = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-        if (userIdClaim != null && Guid.TryParse(userIdClaim.Value, out Guid userId))
+        if (_httpContextAccessor.HttpContext == null)
         {
+            throw new UnauthorizedAccessException();
+        }
 
-            var user = await _context.User
+        var userIdClaim =
+            _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)
+            ?? throw new UnauthorizedAccessException();
+
+        if (!Guid.TryParse(userIdClaim.Value, out var userId))
+        {
+            throw new UnauthorizedAccessException();
+        }
+
+        var user =
+            await _userRepository
+                .GetAll()
                 .Include(u => u.Connections)
-                .FirstOrDefaultAsync(u => u.Id == userId);
+                .FirstOrDefaultAsync(u => u.Id == userId) ?? throw new UserNotFoundException();
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-            user.Connections.Add(new Connection
+        user.Connections.Add(
+            new Connection
             {
-                ConnectionId = Context.ConnectionId,
+                Id = Context.ConnectionId,
                 GroupName = groupName,
                 Connected = true
-            });
+            }
+        );
 
-            await _context.SaveChangesAsync();
-            await Clients.Group(groupName).ReceiveMessage(message);
-        }
+        await _unitOfWork.SaveChangesAsync();
+        await Clients.Group(groupName).ReceiveMessage(message);
     }
 
     public async Task JoinGroup(string groupName)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-        await Clients.Group(groupName).ReceiveMessage($"{Context.ConnectionId} has joined the group {groupName}.");
+        await Clients
+            .Group(groupName)
+            .ReceiveMessage($"{Context.ConnectionId} has joined the group {groupName}.");
     }
-
-
 }
